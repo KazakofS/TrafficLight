4fca081d3d67779298b459e014738a8aad9ff0b8
Sergey <kazakovs1@bk.ru>
2023-01-31 22:52:11 +0500

    create two view

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Colors {
+    case red
+    case yellow
+    case green
+}
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var viewButton: UIButton!
+    
+    private let lightOff: CGFloat = 0.3
+    private let LightOn: CGFloat = 1
+    
+    private var color = Colors.red
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.layer.cornerRadius = redLight.frame.height / 2
+        redLight.alpha = lightOff
+        yellowLight.layer.cornerRadius = yellowLight.frame.height / 2
+        yellowLight.alpha = lightOff
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
+        greenLight.alpha = lightOff
+        viewButton.layer.cornerRadius = 15
     }
-
-
+    
+    @IBAction func startButtonTap() {
+        viewButton.setTitle("NEXT", for: .normal)
+        switch color {
+        case .red:
+            greenLight.alpha = lightOff
+            redLight.alpha = LightOn
+            color = .yellow
+        case .yellow:
+            redLight.alpha = lightOff
+            yellowLight.alpha = LightOn
+            color = .green
+        case .green:
+            yellowLight.alpha = lightOff
+            greenLight.alpha = LightOn
+            color = .red
+        }
+    }
 }
+
+
+
+
 
